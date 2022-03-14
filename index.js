267855de411a0b39e26593ccfc7d2138e8200edb
Morsetim <mauricium.maurice@yahoo.com>
2022-03-14 12:38:12 +0100

    bg cors

--- index.js
+++ index.js
@@ -24,7 +24,17 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(()=>console.log("DB Connection Successful"))
   .catch(err=>console.log(err));
-
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+  //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
+  // Pass to next layer of middleware
+  next();
+});
 app.use(cors(corsOptions))
 app.use(bodyParser.json());
 app.use(express.json())
