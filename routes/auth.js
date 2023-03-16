d3c07d0e20fc548a04c7ba512bb84b513ed36259
Morsetim <mauricium.maurice@yahoo.com>
2023-03-16 15:47:08 +0100

    User exist validation for registered user

--- routes/auth.js
+++ routes/auth.js
@@ -32,7 +32,7 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(401).json("Wrong password or username");
+    !user && res.status(401).json({message:"Wrong password or username", status: 401});
 
     const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
     const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
