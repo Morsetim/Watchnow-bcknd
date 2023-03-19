6593ec1395e53182abce9affce32a6e4caac0acb
Morsetim <mauricium.maurice@yahoo.com>
2023-03-19 10:23:14 +0100

    Sign-in validation for invalid email or password

--- routes/auth.js
+++ routes/auth.js
@@ -32,7 +32,7 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(401).json("Wrong password or username");
+    !user && res.status(401).json("Wrong password or email");
 
     const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
     const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
